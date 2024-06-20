19bcf6aa6d45e5af91c9dea2e0e8f01a3dec283c
gipsywarrior <godricvalleys@gmail.com>
2024-06-20 02:54:05 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,27 @@
+  $(document).ready(function(){
+    $('#id1').click(function(){
+      // Remueve la clase 'active' de todos los párrafos
+      $('.texto div').removeClass('activo');
+      // Agrega la clase 'active' al párrafo con el id 'par1'
+      $('.par1').addClass('activo');
+    });
+  });
+
+  $(document).ready(function(){
+    $('#id2').click(function(){
+      // Remueve la clase 'active' de todos los párrafos
+      $('.texto div').removeClass('activo');
+      // Agrega la clase 'active' al párrafo con el id 'par1'
+      $('.par2').addClass('activo');
+    });
+  });
+
+  $(document).ready(function(){
+    $('#id3').click(function(){
+      // Remueve la clase 'active' de todos los párrafos
+      $('.texto div').removeClass('activo');
+      // Agrega la clase 'active' al párrafo con el id 'par1'
+      $('.par3').addClass('activo');
+    });
+  });
+  
