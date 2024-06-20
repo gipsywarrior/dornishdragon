5e97aa376ad4ba8902e8081fd9ccc40013bf7c64
gipsywarrior <godricvalleys@gmail.com>
2024-06-20 15:26:31 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -25,3 +25,28 @@
     });
   });
   
+  /* Hover del subbody */
+  $(document).ready(function() {
+    $('.subbody.hover').hover(
+        function() {
+            // Cuando el mouse entra en el elemento .subbody.hover
+            $(this).closest('.cont2').css('flex', '2');
+            $(this).find('p').css({
+                'height': '100%',
+                'margin-top': '0px',
+                'padding-block': '30px',
+                'color': 'inherit'
+            });
+        },
+        function() {
+            // Cuando el mouse sale del elemento .subbody.hover
+            $(this).closest('.cont2').css('flex', '1'); // Vuelve al valor original
+            $(this).find('p').css({
+                'height': '',
+                'margin-top': '',
+                'padding-block': '',
+                'color': ''
+            });
+        }
+    );
+});
